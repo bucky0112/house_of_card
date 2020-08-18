81db1081098269e93c63dbd5796445d99c5bd5cb
bucky0112 <bucky0112@gmail.com>
2020-08-19 00:29:37 +0800

    cart page's coupon function done not yet

--- src/views/front/Cart.vue
+++ src/views/front/Cart.vue
@@ -38,10 +38,9 @@
                           <b-icon icon="dash-circle"></b-icon>
                         </b-button>
                       </template>
-                      <input
+                      <b-form-input
                         type="text"
                         class="form-control border-0 text-center my-auto shadow-none bg-light"
-                        placeholder
                         min="1"
                         v-model="item.quantity"
                       />
@@ -59,239 +58,79 @@
                     <p class="mb-0 ml-auto">{{ item.product.price * item.quantity | thousands }}</p>
                   </td>
                   <td class="border-0 align-middle">
-                    <a href="#" @click.prevent="removeCartItem(item.product.id)">
+                    <b-button variant="outline-dark"
+                      @click="removeCartItem(item.product.id)">
                       <b-icon icon="x-circle"></b-icon>
-                    </a>
+                    </b-button>
                   </td>
                 </tr>
               </tbody>
             </table>
-            <div class="input-group w-50">
-              <input
-                type="text"
-                class="form-control rounded-0 border-bottom
-                border-top-0 border-left-0 border-right-0 shadow-none"
-                placeholder="Coupon Code"
-                aria-label="Recipient's username"
-                aria-describedby="button-addon2"
-              />
-              <div class="input-group-append">
-                <button
-                  class="btn btn-outline-dark border-bottom
-                  border-top-0 border-left-0 border-right-0 rounded-0"
-                  type="button"
-                  id="button-addon2"
-                >
-                  <i class="fas fa-paper-plane"></i>
-                </button>
-              </div>
-            </div>
           </div>
           <div class="col-md-4">
             <div class="border p-4 mb-4">
-              <h4 class="font-weight-bold mb-4">Order Detail</h4>
+              <h4 class="font-weight-bold mb-4">詳細訂單內容</h4>
               <table class="table text-muted border-bottom">
                 <tbody>
                   <tr>
-                    <th scope="row" class="border-0 px-0 pt-4 font-weight-normal">Subtotal</th>
-                    <td class="text-right border-0 px-0 pt-4">NT$24,000</td>
+                    <th scope="row" class="border-0 px-0 pt-4 font-weight-normal">全部金額</th>
+                    <td class="text-right border-0 px-0 pt-4">{{ updateCartTotal | thousands }}</td>
                   </tr>
                   <tr>
-                    <th scope="row" class="border-0 px-0 pt-0 pb-4 font-weight-normal">Payment</th>
-                    <td class="text-right border-0 px-0 pt-0 pb-4">ApplePay</td>
+                    <th scope="row" class="border-0 px-0 pt-0 pb-4 font-weight-normal">
+                      <label for="input-coupon">優惠碼</label>
+                    </th>
+                    <td class="text-right border-0 px-0 pt-0 pb-4">
+                      <b-input-group>
+                        <b-form-input
+                          type="text"
+                          id="input-coupon"
+                          v-model="couponCode"
+                          placeholder="請輸入優惠碼"/>
+                      </b-input-group>
+                    </td>
                   </tr>
                 </tbody>
               </table>
               <div class="d-flex justify-content-between mt-4">
                 <p class="mb-0 h4 font-weight-bold">Total</p>
-                <p class="mb-0 h4 font-weight-bold">NT$24,000</p>
+                <p class="mb-0 h4 font-weight-bold">{{ updateCartTotal | thousands }}</p>
               </div>
-              <a href="./checkout.html" class="btn btn-dark btn-block mt-4">Lorem ipsum</a>
-            </div>
-          </div>
-        </div>
-        <div class="my-5">
-          <h3 class="font-weight-bold">Lorem ipsum dolor sit amet</h3>
-          <div class="swiper-container mt-4 mb-5">
-            <div class="swiper-wrapper">
-              <div class="swiper-slide">
-                <div class="card border-0 mb-4 position-relative position-relative">
-                  <img
-                    src="https://images.unsplash.com/photo-1490312278390-ab64016e0aa9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
-                    class="card-img-top rounded-0"
-                    alt="..."
-                  />
-                  <a href="#" class="text-dark"></a>
-                  <div class="card-body p-0">
-                    <h4 class="mb-0 mt-3">
-                      <a href="#">Lorem ipsum</a>
-                    </h4>
-                    <p class="card-text mb-0">
-                      NT$1,080
-                      <span class="text-muted">
-                        <del>NT$1,200</del>
-                      </span>
-                    </p>
-                    <p class="text-muted mt-3"></p>
-                  </div>
-                </div>
-              </div>
-              <div class="swiper-slide">
-                <div class="card border-0 mb-4 position-relative position-relative">
-                  <img
-                    src="https://images.unsplash.com/photo-1490312278390-ab64016e0aa9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
-                    class="card-img-top rounded-0"
-                    alt="..."
-                  />
-                  <a href="#" class="text-dark"></a>
-                  <div class="card-body p-0">
-                    <h4 class="mb-0 mt-3">
-                      <a href="#">Lorem ipsum</a>
-                    </h4>
-                    <p class="card-text mb-0">
-                      NT$1,080
-                      <span class="text-muted">
-                        <del>NT$1,200</del>
-                      </span>
-                    </p>
-                    <p class="text-muted mt-3"></p>
-                  </div>
-                </div>
-              </div>
-              <div class="swiper-slide">
-                <div class="card border-0 mb-4 position-relative position-relative">
-                  <img
-                    src="https://images.unsplash.com/photo-1490312278390-ab64016e0aa9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
-                    class="card-img-top rounded-0"
-                    alt="..."
-                  />
-                  <a href="#" class="text-dark"></a>
-                  <div class="card-body p-0">
-                    <h4 class="mb-0 mt-3">
-                      <a href="#">Lorem ipsum</a>
-                    </h4>
-                    <p class="card-text mb-0">
-                      NT$1,080
-                      <span class="text-muted">
-                        <del>NT$1,200</del>
-                      </span>
-                    </p>
-                    <p class="text-muted mt-3"></p>
-                  </div>
-                </div>
-              </div>
-              <div class="swiper-slide">
-                <div class="card border-0 mb-4 position-relative position-relative">
-                  <img
-                    src="https://images.unsplash.com/photo-1490312278390-ab64016e0aa9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
-                    class="card-img-top rounded-0"
-                    alt="..."
-                  />
-                  <a href="#" class="text-dark"></a>
-                  <div class="card-body p-0">
-                    <h4 class="mb-0 mt-3">
-                      <a href="#">Lorem ipsum</a>
-                    </h4>
-                    <p class="card-text mb-0">
-                      NT$1,080
-                      <span class="text-muted">
-                        <del>NT$1,200</del>
-                      </span>
-                    </p>
-                    <p class="text-muted mt-3"></p>
-                  </div>
-                </div>
-              </div>
-              <div class="swiper-slide">
-                <div class="card border-0 mb-4 position-relative position-relative">
-                  <img
-                    src="https://images.unsplash.com/photo-1490312278390-ab64016e0aa9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
-                    class="card-img-top rounded-0"
-                    alt="..."
-                  />
-                  <a href="#" class="text-dark"></a>
-                  <div class="card-body p-0">
-                    <h4 class="mb-0 mt-3">
-                      <a href="#">Lorem ipsum</a>
-                    </h4>
-                    <p class="card-text mb-0">
-                      NT$1,080
-                      <span class="text-muted">
-                        <del>NT$1,200</del>
-                      </span>
-                    </p>
-                    <p class="text-muted mt-3"></p>
-                  </div>
-                </div>
-              </div>
+              <a href="#" class="btn btn-info btn-block mt-4" @click.passive="useCoupon">使用優惠碼</a>
+              <a href="#" class="btn btn-dark btn-block mt-4">結帳去</a>
             </div>
           </div>
         </div>
       </div>
     </div>
-    <div class="bg-light py-4">
-      <div class="container">
-        <div
-          class="d-flex flex-column flex-md-row
-          justify-content-between align-items-md-center align-items-start"
-        >
-          <p class="mb-0 font-weight-bold">Lorem ipsum dolor sit amet.</p>
-          <div class="input-group w-md-50 mt-md-0 mt-3">
-            <input type="text" class="form-control rounded-0" placeholder />
-            <div class="input-group-append">
-              <button class="btn btn-dark rounded-0" type="button" id="search">Lorem ipsum</button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="bg-dark py-5">
-      <div class="container">
-        <div class="d-flex align-items-center justify-content-between text-white mb-md-7 mb-4">
-          <a class="text-white h4" href="./index.html">LOGO</a>
-          <ul class="d-flex list-unstyled mb-0 h4">
-            <li>
-              <a href="#" class="text-white mx-3">
-                <i class="fab fa-facebook"></i>
-              </a>
-            </li>
-            <li>
-              <a href="#" class="text-white mx-3">
-                <i class="fab fa-instagram"></i>
-              </a>
-            </li>
-            <li>
-              <a href="#" class="text-white ml-3">
-                <i class="fab fa-line"></i>
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div
-          class="d-flex flex-column flex-md-row
-          justify-content-between align-items-md-end align-items-start text-white"
-        >
-          <div class="mb-md-0 mb-1">
-            <p class="mb-0">02-3456-7890</p>
-            <p class="mb-0">service@mail.com</p>
-          </div>
-          <p class="mb-0">© 2020 LOGO All Rights Reserved.</p>
-        </div>
-      </div>
-    </div>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
+import Footer from '@/components/front/Footer.vue';
+
 export default {
+  components: {
+    Footer,
+  },
   data() {
     return {
-      cart: {},
+      cart: [],
       isLoading: false,
+      cartTotal: 0,
+      coupon: {},
+      couponCode: '',
     };
   },
   created() {
     this.getCart();
+  },
+  computed: {
+    updateCartTotal() {
+      // 第一次處理帶入初始值 0
+      return this.cart.reduce((accumulator, i) => accumulator + i.product.price * i.quantity, 0);
+    },
   },
   methods: {
     getCart() {
@@ -349,6 +188,22 @@ export default {
           this.isLoading = false;
           this.$bus.$emit('updateCart');
           this.getCart();
+        })
+        .catch(() => {
+          this.isLoading = false;
+        });
+    },
+    useCoupon() {
+      this.isLoading = true;
+      const url = `${process.env.VUE_APP_APIPATH}/api/${process.env.VUE_APP_UUID}/ec/coupon/search`;
+      this.axios
+        .post(url, {
+          code: this.couponCode,
+        })
+        .then((res) => {
+          this.coupon = res.data.data;
+          console.log(this.coupon);
+          this.isLoading = false;
         })
         .catch(() => {
           this.isLoading = false;
